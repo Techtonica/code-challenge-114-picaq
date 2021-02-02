c5d388a924a1a55da54fe12bd26e829cfa4460ec
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-02 09:39:29 -0800

    add space

--- challenges/114.js
+++ challenges/114.js
@@ -1,6 +1,6 @@
 // ### Flattening
 //
-// Use the existing Array \`reduce\` method in combination with the \`concat\` method to “flatten”an array of arrays into a single array that has all the elements of the original arrays.
+// Use the existing Array \`reduce\` method in combination with the \`concat\` method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
 // You must name the function "flatten".
 
 // e.g.
